3a376a9785cf33eecbbc57382d21984cbd2fddfe
DavidPaniagua15 <davidpaniagua15U@outlook.com>
2022-10-23 19:55:03 -0600

    question1

---
+++ JS/questions.js
@@ -0,0 +1,9 @@
+// this is the list of all the questions and answers //
+var questions = [
+    {
+        title: 'The marathon goes back to:',
+        choices: ['the Roman wagon race', 'A persian tradition', 'the days of ancient Greece'],
+        answer: 'the days of ancient Greece',
+    },
+    
+]
