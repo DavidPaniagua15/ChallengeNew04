725d4445e329f45fcd7ec450c9da219979ce1352
DavidPaniagua15 <davidpaniagua15U@outlook.com>
2022-10-23 20:17:36 -0600

    question3

--- JS/questions.js
+++ JS/questions.js
@@ -13,5 +13,12 @@ var questions = [
         answer: 'a lot of carbohydrates',
     },
 
+
+    {
+        title: 'Finish the sentence: Running :',
+        choices: ['makes your heart stronger', 'helps you put on weight', 'helps imporve the number of white blood cells'],
+        answer: 'makes your heart stronger',
+    },
+
     
 ]
