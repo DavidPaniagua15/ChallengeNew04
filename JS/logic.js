ec10f5a6c9da7e5608c30b69b95939f11c8c7680
DavidPaniagua15 <davidpaniagua15U@outlook.com>
2022-11-06 11:31:30 -0700

    variables

--- JS/logic.js
+++ JS/logic.js
@@ -3,3 +3,7 @@ var currentQuestionIndex = 0;
 var time = questions.length * 20;
 var timeId;
 
+// DOM elements var //
+var questionsEl = document.getElementById('questions');
+var timerEl = document.getElementById('time');
+var choicesEl = document.getElementById('choices');
