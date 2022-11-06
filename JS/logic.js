cb093c15370e6f030c93c8f885123d82b4097877
DavidPaniagua15 <davidpaniagua15U@outlook.com>
2022-11-06 11:34:12 -0700

    btn

--- JS/logic.js
+++ JS/logic.js
@@ -7,3 +7,6 @@ var timeId;
 var questionsEl = document.getElementById('questions');
 var timerEl = document.getElementById('time');
 var choicesEl = document.getElementById('choices');
+var submitBtn = document.getElementById('submit');
+var startBtn = document.getElementById('start');
+var initialsEl = document.getElementById('initials');
